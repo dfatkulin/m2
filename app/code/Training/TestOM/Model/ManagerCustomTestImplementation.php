9ee9286ba7ab8f3b30804eeef6752d1f1c3455f5
Dimitri <dimitri@MacBook-Pro-van-Dimitri.local>
2021-02-19 18:55:52 +0100

    Задание 1.6

---
+++ app/code/Training/TestOM/Model/ManagerCustomTestImplementation.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Training\TestOM\Model;
+
+class ManagerCustomTestImplementation implements ManagerTestInterface
+{
+public function set()
+{
+// custom logic
+}
+public function get()
+{
+// custom logic
+}
+}
