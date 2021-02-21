3adbf9ee7544845ddfb80752306097b18124d776
Dimitri <dimitri@MacBook-Pro-van-Dimitri.local>
2021-02-21 19:13:43 +0100

    Задание 1.10

---
+++ app/code/Training/Test/Observer/RedirectToLogin.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Training\Test\Observer;
+
+use Magento\Framework\App\ActionFlag;
+use Magento\Framework\App\Response\RedirectInterface;
+use Magento\Framework\Event\Observer;
+use Magento\Framework\Event\ObserverInterface;
+
+class RedirectToLogin implements ObserverInterface
+{
+    /**
+     * @var RedirectInterface
+     */
+    private $redirect;
+    /**
+     * @var ActionFlag
+     */
+    private $actionFlag;
+
+    /**
+     * RedirectToLogin constructor.
+     * @param RedirectInterface $redirect
+     * @param ActionFlag $actionFlag
+     */
+    public function __construct(
+        RedirectInterface $redirect,
+        ActionFlag $actionFlag
+    ) {
+        $this->redirect = $redirect;
+        $this->actionFlag = $actionFlag;
+    }
+
+    /**
+     * @param Observer $observer
+     */
+    public function execute(Observer $observer)
+    {
+        $request = $observer->getEvent()->getData('request');
+        if ($request->getModuleName() == 'catalog'
+            && $request->getControllerName() == 'product'
+            && $request->getActionName() == 'view'
+        ) {
+            $controller = $observer->getEvent()->getData('controller_action');
+            $this->actionFlag->set('', \Magento\Framework\App\ActionInterface::FLAG_NO_DISPATCH,
+                true);
+            $this->redirect->redirect($controller->getResponse(), 'customer/account/login');
+        }
+    }
+}
